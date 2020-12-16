b19f83e30ecad25239a87a8ff63d0c5d3e712674
Fredrik Pettersson <fredrik.pettersson@lotto24.de>
2020-12-16 15:58:02 +0000

    Add competition model

---
+++ src/competition.ts
@@ -0,0 +1,51 @@
+export interface Competition {
+  id?: string;
+  description: string;
+  cityName: string;
+  countryName: string;
+  startTime: Date;
+  imageUrl: string;
+  actIds: string[];
+}
+
+export const competitionSchema = {
+  required: ['description', 'cityName', 'countryName', 'startTime', 'imageUrl', 'actIds'],
+  properties: {
+    id: {
+      type: 'string',
+    },
+    description: {
+      type: 'string',
+    },
+    cityName: {
+      type: 'string',
+    },
+    countryName: {
+      type: 'string',
+    },
+    startTime: {
+      type: 'string',
+      format: 'date-time',
+    },
+    imageUrl: {
+      type: 'string',
+      format: 'uri',
+    },
+    actIds: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+    },
+  },
+};
+
+export const defaultCompetition: Competition = {
+  id: '84ce3ed6-a479-4ba4-86e1-d053638fd8a1',
+  description: 'Quarterfinal',
+  cityName: 'Stockholm',
+  countryName: 'Sweden',
+  startTime: new Date(),
+  imageUrl: 'https://imageurl.example.com/img.jpg',
+  actIds: ['84ce3ed6-a479-4ba4-86e1-d053638fd8a1'],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { Act, actSchema, defaultAct } from './act';
+export { Competition, competitionSchema, defaultCompetition } from './competition';
