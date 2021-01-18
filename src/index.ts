48f495cb5ea7e83ab17902036db688210828a24e
Fredrik Pettersson <fredrik.pettersson@lotto24.de>
2021-01-18 19:57:59 +0000

    Add InitiateAvatarUploadResponse

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Act, actSchema, defaultAct } from './act';
 export { Competition, competitionSchema, defaultCompetition } from './competition';
+export { InitiateAvatarUploadResponse } from './initiate-avatar-upload-response';
 export { defaultUser, User, userSchema } from './user';
---
+++ src/initiate-avatar-upload-response.ts
@@ -0,0 +1,3 @@
+export interface InitiateAvatarUploadResponse {
+  signedUrl: string;
+}
